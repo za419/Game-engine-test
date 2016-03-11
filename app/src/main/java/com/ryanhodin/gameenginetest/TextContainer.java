3f0bc861f03ba4bc820be4889f366815fb4bcecb
Ryan Hodin <ryan951357@gmail.com>
2016-03-10 22:10:28 -0600

    Add callback system to text changer

--- app/src/main/java/com/ryanhodin/gameenginetest/TextContainer.java
+++ app/src/main/java/com/ryanhodin/gameenginetest/TextContainer.java
@@ -1,34 +1,41 @@
 package com.ryanhodin.gameenginetest;
 
 public class TextContainer {
-	protected String mText;
-	protected Runnable mCallback;
-	protected boolean mCallbackReady;
+	protected String mText; // The contained text
+	protected Callable mCallback=null; // The callback for when the text is changed
+	protected boolean mCallbackReady; // False if a callback is in progress
+	protected boolean mCallbackWaiting; // True if a callback is waiting to run
 
 	public TextContainer() {
 		mText="";
 		mCallbackReady=true;
+		mCallbackWaiting=false;
 	}
 
 	public TextContainer(String text) {
 		mText=text;
 		mCallbackReady=true;
+		mCallbackWaiting=false;
 	}
 
-	public TextContainer(Runnable callback) {
+	public TextContainer(Callable callback) {
 		mCallback=callback;
 		mCallbackReady=true;
+		mCallbackWaiting=false;
 	}
 
-	public TextContainer(String text, Runnable callback) {
+	public TextContainer(String text, Callable callback) {
 		mText=text;
 		mCallback=callback;
 		mCallbackReady=true;
+		mCallbackWaiting=false;
 	}
 
 	public TextContainer(TextContainer copy) {
 		mText=copy.mText;
-		mCallbackReady=true;
+		mCallback=copy.mCallback;
+		mCallbackReady=true; // This allows two calls in parallel
+		mCallbackWaiting=false;
 	}
 
 	public String text() {
@@ -36,8 +43,28 @@ public class TextContainer {
 	}
 
 	public String text (String replacement) {
-		String out=mText;
+		final String out=mText;
 		mText=replacement;
+		Thread th=new Thread(new Runnable() {
+			@Override
+			public void run() {
+				if (mCallback==null || mCallbackWaiting)
+					return;
+				try {
+					if (!mCallbackReady)
+						mCallbackWaiting=true;
+					while (!mCallbackReady)
+						Thread.sleep(16);
+					mCallbackReady=false;
+					mCallback.call(out, mText);
+					mCallbackReady=true;
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				mCallbackWaiting=false;
+			}
+		});
+		th.start();
 		return out;
 	}
 
@@ -53,5 +80,10 @@ public class TextContainer {
 
 	public static String toString(TextContainer tc) {
 		return tc.toString();
+	}
+
+	public abstract static class Callable { // Used as a callback for when text changes
+		// Called with the text pre- and post- the change for which this is called
+		public abstract void call(String old, String replacement);
 	}
 }
