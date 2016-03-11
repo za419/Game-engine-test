3bf616966d92759d44074061be662851d0ffcf12
Ryan Hodin <ryan951357@gmail.com>
2016-03-10 21:56:02 -0600

    Add callback ready flag and string accessors

--- app/src/main/java/com/ryanhodin/gameenginetest/TextContainer.java
+++ app/src/main/java/com/ryanhodin/gameenginetest/TextContainer.java
@@ -3,26 +3,42 @@ package com.ryanhodin.gameenginetest;
 public class TextContainer {
 	protected String mText;
 	protected Runnable mCallback;
+	protected boolean mCallbackReady;
 
 	public TextContainer() {
 		mText="";
+		mCallbackReady=true;
 	}
 
-	public TextContainer(final String text) {
+	public TextContainer(String text) {
 		mText=text;
+		mCallbackReady=true;
 	}
 
 	public TextContainer(Runnable callback) {
 		mCallback=callback;
+		mCallbackReady=true;
 	}
 
-	public TextContainer(final String text, Runnable callback) {
+	public TextContainer(String text, Runnable callback) {
 		mText=text;
 		mCallback=callback;
+		mCallbackReady=true;
 	}
 
 	public TextContainer(TextContainer copy) {
 		mText=copy.mText;
+		mCallbackReady=true;
+	}
+
+	public String text() {
+		return mText;
+	}
+
+	public String text (String replacement) {
+		String out=mText;
+		mText=replacement;
+		return out;
 	}
 
 	@Override
